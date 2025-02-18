eacf8e05dde21177aaab128c109816c555fd8627
yaroslavom <yarykomm@gmail.com>
2025-02-18 23:42:29 +0100

    Updates

--- src/events/expiration-complete-event.ts
+++ src/events/expiration-complete-event.ts
@@ -1,7 +1,7 @@
 import { Subjects } from './subjects';
 
 export interface ExpirationCompleteEvent {
-    expiration: Subjects.ExpirationComplete;
+    subject: Subjects.ExpirationComplete;
     data: {
         orderId: string;
     };
