2dd5f02bc04c0ec8f8846422892645327f84b25d
yaroslavom <yarykomm@gmail.com>
2025-02-20 12:53:15 +0100

    Updates

--- src/events/index.ts
+++ src/events/index.ts
@@ -6,5 +6,6 @@ export * from './ticket-updated-event';
 export * from './order-created-event';
 export * from './order-cancelled-event';
 export * from './expiration-complete-event';
+export * from './payment-created-event';
 
 export { OrderStatus } from './types/order-status';
